5a023827631cc93dd0d6f7f0f14853f0db71ce83
latelierdesretouches <64561315+latelierdesretouches@users.noreply.github.com>
2022-04-23 03:12:25 +0200

    Add files via upload

--- player.py
+++ player.py
@@ -1,12 +1,14 @@
+from typing_extensions import Self
 import pygame
-
+from projectile import Projectile
 #creer classe player
+
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
-		self.velocity=1
-		super().__init__()
-		self.image=pygame.image.load('assets\player.png')
-		self.rect=self.image.get_rect()
-		self.rect.x=400
-		self.rect.y=500
-    
+        super().__init__()
+		self.velocity = 1
+        self.all_projectiles = pygame.sprite.Group()
+		self.rect.x = 400
+		self.rect.y = 500
+		self.image = pygame.image.load('assets\player.png')
+		self.rect = self.image.get_rect()
